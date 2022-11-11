38ca92753507e943da8229743911dc80fd066034
matheussf100 <matheussf300@gmail.com>
2022-11-11 14:49:12 -0300

    Funções (básico)

---
+++ Aula16/index.js
@@ -0,0 +1,28 @@
+const raiz = (n) => n ** 0.5;
+
+console.log(raiz(9))
+console.log(raiz(16))
+console.log(raiz(25))
+
+/*function soma(x = 1, y = 2) {
+    const resultado = x + y;
+    return resultado;
+}
+
+const resultado = soma()
+console.log(soma(2, 2))
+console.log(resultado)*/
+
+// console.log(soma(2, 2));
+// console.log(soma(3, 2));
+// console.log(soma(5, 2));
+
+/*function saudacao(nome){
+    // console.log(`Bom dia! ${nome}`)
+    return `Bom dia ${nome}!`;
+}
+*/
+//const variavel = saudacao('Luiz');
+//// console.log(variavel);
+//saudacao('Luiz');
+//saudacao('Maria')
