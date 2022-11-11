ec7de8292daa82872e106770820ef00e956b05de
matheussf100 <matheussf300@gmail.com>
2022-11-11 01:00:39 -0300

    Arrays

---
+++ Aula15/index.js
@@ -0,0 +1,39 @@
+//               0       1         2
+const alunos = ['Luiz', 'Nelson', 'Vanderlei'];
+
+alunos.push('Luiza')
+alunos.push('Eduardo')
+
+console.log(typeof alunos);
+console.log(alunos instanceof Array)
+
+//console.log(alunos.slice(0, 3));
+//console.log(alunos[55])
+
+//delete alunos[1]
+//console.log(alunos)
+
+//console.log(removido)
+//alunos.pop();
+//console.log(alunos);
+
+// alunos.unshift('Luiza');
+// alunos.unshift('fabin');
+
+
+// alunos.push('Robertin'); Adiciona no fim.
+// alunos.push('Adailton'); Adiciona no fim.
+
+// alunos[0] = 'Eduardo'; Altera 
+// alunos[3] = 'Jadeci'; Adiciona
+
+/*alunos[alunos.length] = 'Luiza';
+alunos[alunos.length] = 'Marcelino';
+alunos[alunos.length] = 'jorgin';
+*/
+
+
+/*console.log(alunos);
+console.log(alunos[0]);
+console.log(alunos[2]);
+*/
