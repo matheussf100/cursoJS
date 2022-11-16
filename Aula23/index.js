64f7a2570abfaf7c0c54cbcd9885fa1f2a783aad
matheussf100 <matheussf300@gmail.com>
2022-11-16 13:12:22 -0300

    lógica básica - if, else if e else

---
+++ Aula23/index.js
@@ -0,0 +1,22 @@
+/*
+Entre 0 - 11 -> Bom dia 
+Entre 12 - 17 -> Boa tarde
+Entre 18 - 23 -> Boa noite
+*/
+
+// If pode ser usado sozinho.
+// Sempre que utilizo a palavra else, preciso de um if antes.
+// Eu posso ter vários else if's na checagem.
+// Só posso ter um else na checagem.
+
+const hora = 30;
+
+if (hora >= 0 && hora <= 11) {
+    console.log('Bom dia');
+} else if (hora >= 12 && hora <= 17) {
+    console.log('Boa tarde');
+} else if (hora >=18 && hora <= 23){
+    console.log('Boa noite');
+} else {
+console.log('horário invalido!');
+}
