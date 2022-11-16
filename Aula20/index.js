2b341982d57e42f36b175a5c4056d0adc501c8e0
matheussf100 <matheussf300@gmail.com>
2022-11-15 21:06:49 -0300

    Lógica básica - Operadores de comparação

---
+++ Aula20/index.js
@@ -0,0 +1,11 @@
+/*
+Operadores de comparação
+> maior que
+>= maior que ou igual a (qualquer uma das expressões que for verdadeiro retorna verdadeiro.)
+< menor que 
+<= menor que ou igual a (qualquer uma das expressões que for verdadeiro retorna verdadeiro.)
+== igualdade (valor) *Não utilizado*
+=== igualdade estrita (valor e tipo)
+!= diferente (valor) * Não utilizado*
+!== diferente estrito (valor e tipo)
+*/
