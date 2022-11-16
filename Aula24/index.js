961bbf66f2288e21fffb3495aa30ad4bb94457f6
matheussf100 <matheussf300@gmail.com>
2022-11-16 13:51:22 -0300

    Lógica básica - if, else if, else (2).

---
+++ Aula24/index.js
@@ -0,0 +1,11 @@
+const num = -1;
+
+if (num >= 0 && num <=5) {
+    console.log('O número está entre 0 e 5.');
+} else if (num >=6 && num <=8) {
+    console.log('O número está entre 6 e 8.');
+} else if (num >=9 && num <=11) {
+    console.log('O número está entre 9 e 11.');
+} else {
+    console.log('O número NÃO está entre 0 e 11.')
+}
