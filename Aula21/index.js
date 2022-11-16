73c578e036e4080f95176ff275028d8be7281aa4
matheussf100 <matheussf300@gmail.com>
2022-11-16 12:29:00 -0300

    Lógica básica - Operadores Lógicos.

---
+++ Aula21/index.js
@@ -0,0 +1,17 @@
+/*
+Operadores Lógicos
+&& -> AND -> E -> Todas as expressões precisam ser verdadeiras para retorarem true.
+|| -> OR -> OU
+! -> NOT -> NÃO
+*/
+// const expressaoAnd = true && true && true && true
+//const expressaoOr = false || false || false || true;
+//console.log(expressaoOr);
+/*
+const usuario = 'Nelson';
+const senha = '12345678';
+
+const loguin = usuario === 'Nelson' && senha === '1234567';
+console.log(loguin);
+*/
+console.log(!true)
