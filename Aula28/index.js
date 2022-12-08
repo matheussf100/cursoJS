077d9a918f2fbbe700e84cd3033e12cdf4bbd056
matheussf100 <matheussf300@gmail.com>
2022-12-08 19:15:15 -0300

    Aula Switch/Case + rescunho

---
+++ Aula28/index.js
@@ -0,0 +1,43 @@
+const data = new Date('1997-10-25 00:00:00');
+const diaSemana = data.getDay();
+let diaSemanaTexto;
+switch (diaSemana) {''
+   case 0:
+      diaSemanaTexto = 'Domingo';
+      break;
+   case 1:
+      diaSemanaTexto = 'Segunda';
+      break;
+   case 2:
+      diaSemanaTexto = 'Terça';
+      break;
+   case 3:
+      diaSemanaTexto = 'Quarta';
+      break;
+   case 4:
+      diaSemanaTexto = 'Quinta';
+      break;
+   case 5:
+      diaSemanaTexto = 'Sexta';
+      break;
+   case 6:
+      diaSemanaTexto = 'Sábado';
+      break;
+}
+/*  if (diaSemana === 0) {
+    diaSemanaTexto = 'Domingo';
+   } else if (diaSemana === 1) {
+    diaSemanaTexto = 'Segunda'
+   } else if (diaSemana === 2) {
+    diaSemanaTexto = 'Terça';
+   } else if (diaSemana === 3) {
+    diaSemanaTexto = 'Quarta';
+   } else if (diaSemana === 4) {
+    diaSemanaTexto = 'Quinta';
+   } else if (diaSemana === 5) {
+    diaSemanaTexto = 'Sexta';
+   } else if (diaSemana === 6) {
+    diaSemanaTexto = 'Sábado';
+   }
+*/
+console.log(diaSemana, diaSemanaTexto);
