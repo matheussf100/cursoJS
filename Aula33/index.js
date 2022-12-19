47fc247c21cfe327a2bbaa5ec56d1320083a0c3d
matheussf100 <matheussf300@gmail.com>
2022-12-19 17:16:20 -0300

    looping de repetição (for) + Exercicio

---
+++ Aula33/index.js
@@ -0,0 +1,25 @@
+/*console.log('linha 0')
+console.log('linha 01')
+console.log('linha 02')
+console.log('linha 03')
+console.log('linha 04')
+console.log('linha 05')
+*/
+
+// i - index
+/*for (let i = 0; i<=5; i++) {
+    console.log(`Linha ${i}`);
+}
+*/
+
+/*for (let i = 0; i <=10; i++) {
+    const par = i % 2 === 0 ? 'par' : 'impar';
+    console.log(i, par);
+}
+*/
+
+const frutas = ['Maçã', 'Pera', 'Uva'];
+
+for (let i = 0; i < frutas.length; i++) {
+    console.log(frutas[i]);
+}
---
+++ Aula34/exercicio/modelo/assets/js/main.js
@@ -0,0 +1,5 @@
+const elementos = [
+    {tag: 'p', texto: 'Frase 1'},
+    {tag: 'div', texto: 'Frase 2'},
+    {tag: 'footer', texto: 'Frase 4'},
+];
